27bb91b0eb615d04e5bb9ae6f6fc154a02753ca0
1972827581@qq.com <1972827581@qq.com>
2020-11-19 11:06:49 +0800

    springboot相关学习

---
+++ jwt-study/src/main/java/com/lu/jwt/JwtApplication.java
@@ -0,0 +1,18 @@
+package com.lu.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ * @author lzs
+ * @version 1.0
+ * @date 2020/11/19 10:41
+ */
+@SpringBootApplication
+public class JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class,args);
+    }
+}
---
+++ mybatis-plus-study/src/main/java/com/lu/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,18 @@
+package com.lu.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *主启动类
+ * @author lzs
+ * @version 1.0
+ * @date 2020/11/19 11:00
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class,args);
+    }
+}
